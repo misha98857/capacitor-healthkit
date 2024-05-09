f6c33375dccde59cf11c4200cfd025481d422174
Bin Xiao <binxiaosyd@gmail.com>
2024-05-09 22:15:39 +1000

    deleted old dist files

--- dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { CapacitorHealthkitPlugin } from './definitions';
-declare const CapacitorHealthkit: CapacitorHealthkitPlugin;
-export * from './definitions';
-export { CapacitorHealthkit };
--- dist/esm/index.js
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const CapacitorHealthkit = registerPlugin('CapacitorHealthkit', {
-    web: () => import('./web').then(m => new m.CapacitorHealthkitWeb()),
-});
-export * from './definitions';
-export { CapacitorHealthkit };
-//# sourceMappingURL=index.js.map
